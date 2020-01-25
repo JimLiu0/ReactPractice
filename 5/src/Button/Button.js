fd7aa37bd93c10f152f649ac321dd9508a9a1963
JimBerkeley <jim.liu@berkeley.edu>
2020-01-25 14:12:30 -0500

    Add styled-components styling to buttons

--- 5/src/Button/Button.js
+++ 5/src/Button/Button.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+    background-color: lavender
+`;
 
 class Button extends Component{
     render() {
         return (
-            <button onClick={this.props.clicked}>{this.props.text}</button>
+            <StyledButton onClick={this.props.clicked}>{this.props.text}</StyledButton>
         );
     }
 }
